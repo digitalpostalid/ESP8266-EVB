d3acb46655deefee6f79eda79a5f63c709e4e777
digitalpostalid <digitalpostalid@gmail.com>
2018-05-20 10:39:49 -0600

    Add files via upload

---
+++ sample_wifi_test.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid     = "ssid";
+const char* password = "password";
+ 
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Netmask: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+}
+ 
+ 
+void loop() {
+
+}
+
